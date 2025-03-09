d844301ee125c6a5ab1883442922a0d56b807138
Nick Winsen <nwinsen@ncsu.edu>
2025-03-09 19:48:58 -0400

    update app file

--- app/app.py
+++ app/app.py
@@ -1,21 +1,22 @@
-#!flask/bin/python
-from flask import Flask, jsonify,request
-import flask
-import json
+from flask import Flask, jsonify, request
 import json_parser
 import topics_matcher
 
+
 app = Flask(__name__)
 
-@app.route('/match_topics',methods=['POST'])
+@app.route('/match_topics', methods=['POST'])
 def topic_matching():
-	input_parsed_model = json_parser.JsonParser(request.json)
-	assigned_topics_model = topics_matcher.TopicsMatcher(input_parsed_model.student_ids,
-		input_parsed_model.topic_ids,
-		input_parsed_model.student_priorities_dict,
-		input_parsed_model.topic_priorities_dict,
-		input_parsed_model.max_accepted_proposals)
-	return jsonify(assigned_topics_model.get_student_topic_matches())
+
+    input_parsed_model = json_parser.JsonParser(request.json)
+    assigned_topics_model = topics_matcher.TopicsMatcher(
+        input_parsed_model.student_ids,
+        input_parsed_model.topic_ids,
+        input_parsed_model.student_priorities_dict,
+        input_parsed_model.topic_priorities_dict,
+        input_parsed_model.max_accepted_proposals
+    )
+    return jsonify(assigned_topics_model.get_student_topic_matches())
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
